a3ce33955a682e3958929255c9cadf181de42346
dd <140358105+ddurandev@users.noreply.github.com>
2023-11-15 18:19:47 -0500

    initial-commit

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  deleteUser,
+  updateUser,
+  addFriend,
+  removeFriend,
+} = require('../../controllers/userController');
+
+
+router.route('/').get(getUsers).post(createUser);
+
+
+router.route('/:userId').get(getSingleUser).delete(deleteUser).put(updateUser);
+
+
+router.route('/:userId/friends').post(addFriend);
+
+
+router.route('/:userId/friends/:friendId').delete(removeFriend);
+
+module.exports = router;
